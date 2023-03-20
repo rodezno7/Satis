9b5cd74eaf96f10b0488e951f98b6c08aa1cbedf
Arquímides Martínez <amartinez@devtch.com>
2023-03-20 16:02:57 -0600

    Modify agl invoice print format

--- resources/views/sale_pos/receipts/invoice_agl.blade.php
+++ resources/views/sale_pos/receipts/invoice_agl.blade.php
@@ -2,7 +2,7 @@
 	div#container { width: 100%;  font-size: 7.5pt;
 		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; }
 	div#container { width: 13cm; margin: 0cm 0.7cm 0cm 0.7cm; }
-	div#head { height: 3cm; width: 100%; }
+	div#head { height: 3.3cm; width: 100%; }
 	div#header { height: 1.2cm; width: 100%; }
 	div#container table{ width: 100%; }
 	div#container table td { vertical-align: middle; }
@@ -11,7 +11,7 @@
 	div#container div#header table td { height: 0.4cm; }
 	div#details table#sell_lines,
 	div#header table { table-layout: fixed; }
-	div#details table#sell_lines thead th { height: 0.75cm; }
+	div#details table#sell_lines thead th { height: 0.78cm; }
 	div#details table#sell_lines tbody td  { height: 0.57cm; padding-left: 0.1cm; }
 	div#footer { height: 2.4cm; }
 	div#footer table td { padding-left: 0.1cm; }
@@ -23,15 +23,15 @@
 	<div id="header">
 		<table id="header">
 			<tr>
-				<td style="padding-left: 1.5cm; width: 9.5cm" class="cutter">{{ $receipt_details->customer_name }}</td>
-				<td style="padding-left: 1.2cm; width: 3.5cm">{{ @format_date($receipt_details->invoice_date) }}</td>
+				<td style="padding-left: 1.5cm; width: 9.5cm; vertical-align: middle;" class="cutter">{{ $receipt_details->customer_name }}</td>
+				<td style="padding-left: 1.2cm; width: 3.5cm; vertical-align: middle;">{{ @format_date($receipt_details->invoice_date) }}</td>
 			</tr>
 			<tr>
-				<td colspan="2" style="padding-left: 1.5cm;" class="cutter">{{ $receipt_details->customer_location }}</td>
+				<td colspan="2" style="padding-left: 1.5cm; vertical-align: middle;" class="cutter">{{ $receipt_details->customer_location }}</td>
 			</tr>
 			<tr>
-				<td style="padding-left: 2.5cm;" class="cutter">{{ $receipt_details->customer_employee_name }}</td>
-				<td style="padding-left: 1.5cm;">{{ $receipt_details->customer_nit }}</td>
+				<td style="padding-left: 2.5cm; vertical-align: middle;" class="cutter">{{ $receipt_details->customer_employee_name }}</td>
+				<td style="padding-left: 1.5cm; vertical-align: middle;">{{ $receipt_details->customer_nit }}</td>
 			</tr>
 		</table>
 	</div>
